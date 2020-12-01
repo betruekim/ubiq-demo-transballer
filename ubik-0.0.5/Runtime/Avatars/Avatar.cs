d37511ba1cb2378ec57a2ce6a21058db39dad78d
Yacoub Ahmed <yacoub.ahmedy@gmail.com>
2020-12-01 15:23:07 +0000

    Bumped ubik to 0.0.5

---
+++ ubik-0.0.5/Runtime/Avatars/Avatar.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ubik.Messaging;
+using UnityEngine.Events;
+using Ubik.Rooms;
+using Ubik.Dictionaries;
+
+namespace Ubik.Avatars
+{
+    public class Avatar : MonoBehaviour, INetworkObject
+    {
+        public string guid;
+
+        public NetworkId Id { get; } = new NetworkId();
+
+        [Serializable]
+        public class AvatarEvent : UnityEvent<Avatar> { }
+
+        public AvatarEvent OnUpdated;
+        public SerializableDictionary Properties;
+
+        private void Awake()
+        {
+            if (OnUpdated == null)
+            {
+                OnUpdated = new AvatarEvent();
+            }
+        }
+
+        /// <summary>
+        /// Indicates the avatar was instantiated to represent a player on this computer. This flag is informational only. Child components do not have to use it.
+        /// </summary>
+        public bool local;
+
+        private void Reset()
+        {
+            local = false; // better not to transmit by accident than to transmit by accident!
+            guid = Guid.NewGuid().ToString();
+        }
+
+        public void Merge(SerializableDictionary properties)
+        {
+            if(Properties.Update(properties))
+            {
+                OnUpdated.Invoke(this);
+            }
+        }
+    }
+}
---
+++ ubik-0.0.5/Samples~/Introduction/Scripts/AvatarMenuController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Ubik.Avatars;
+using UnityEngine;
+
+namespace Ubik.Samples
+{
+    public class AvatarMenuController : MonoBehaviour
+    {
+        public GameObject[] avatars;
+        public AvatarManager manager;
+
+        public void ChangeAvatar(GameObject prefab)
+        {
+            if (Application.isPlaying)
+            {
+                manager.CreateLocalAvatar(prefab);
+            }
+        }
+
+        public TexturedAvatar GetSkinnable()
+        {
+            if (manager.LocalAvatar)
+            {
+                return manager.LocalAvatar.GetComponent<TexturedAvatar>();
+            }
+            return null;
+        }
+    }
+}
