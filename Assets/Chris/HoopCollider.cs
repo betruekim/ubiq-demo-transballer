db79e5a12d13a7ae156cec8f8428ddfbafb5aba5
Chris Obasi <christopher@autolomous.com>
2020-12-07 12:05:39 +0000

    Added ball spawner and hoops

---
+++ Assets/Chris/HoopCollider.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoopCollider : MonoBehaviour
+{
+    public GameObject hoop;
+    public int colliderNumber;
+
+
+    private Hoop hoopRef;
+    private List<string> gate1;
+
+    void Start()
+    {
+        hoop.transform.Find("BallDisplay").GetComponent<TextMesh>().text = string.Format("{0}", hoop.GetComponent<Hoop>().ballsToGo);
+        hoopRef = hoop.GetComponent<Hoop>();
+        gate1 = hoopRef.gate1;
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        // Check if object name is ball 
+        if (collision.gameObject.name.Contains("Ball(Clone)"))
+        {
+            Hoop hoopRef = hoop.GetComponent<Hoop>();
+            if (colliderNumber == 1)
+            {
+                gate1.Add(collision.gameObject.name);
+            }
+            else if (colliderNumber == 2)
+            {
+                if (gate1.Contains(collision.gameObject.name))
+                {
+                    // Decrement balls left 
+                    hoopRef.ballsToGo -= 1;
+                    hoop.transform.Find("BallDisplay").GetComponent<TextMesh>().text = string.Format("{0}", hoopRef.ballsToGo);
+                    collision.gameObject.SetActive(false);
+
+                    if (hoopRef.ballsToGo == 0)
+                    {
+                        GameObject.Destroy(hoop);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Chris/RandomColour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
+
+public class RandomColour : MonoBehaviour
+{
+    [SerializeField] public Renderer ball;
+    private Color[] colours = new Color[] { Color.green, Color.blue, Color.cyan, Color.green, Color.magenta, Color.red, Color.yellow };
+
+    void Start()
+    {
+        Random rand = new Random();
+        ball.material.color = colours[rand.Next(colours.Length)];
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision with trigger: " + other.gameObject.name);
+    }
+
+}
