316b433c4e3d88e4c2a2c138bfdd1534b01ff602
Yacoub Ahmed <yacoub.ahmedy@gmail.com>
2020-12-15 20:24:12 +0000

    Collision bugfixes and adding remove back

---
+++ Assets/Movement/CustomUser.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Ubik.XR
+{
+    public class CustomUser : MonoBehaviour
+    {
+        public List<Collider> contacted;
+        public List<IUseable> used;
+
+        private HandController controller;
+
+        private void Awake()
+        {
+            controller = GetComponent<HandController>();
+            contacted = new List<Collider>();
+            used = new List<IUseable>();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            controller.TriggerPress.AddListener(Use);
+        }
+
+        void Use(bool state)
+        {
+            if (state)
+            {
+                foreach (var item in contacted)
+                {
+                    if (item == null)
+                    {
+                        contacted.Remove(item);
+                        continue;
+                    }
+                    foreach (var component in item.GetComponentsInParent<MonoBehaviour>())
+                    {
+                        if (component is IUseable)
+                        {
+                            var useable = (component as IUseable);
+                            useable.Use(controller);
+                            used.Add(useable);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                foreach (var item in used)
+                {
+                    item.UnUse(controller);
+                }
+                used.Clear();
+            }
+        }
+
+        // *this* collider is the trigger
+        private void OnTriggerEnter(Collider collider)
+        {
+            foreach (var component in collider.gameObject.GetComponentsInParent<MonoBehaviour>())
+            {
+                if (component is IUseable)
+                {
+                    if (!contacted.Contains(collider))
+                    {
+                        contacted.Add(collider);
+                    }
+                    return;
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider collider)
+        {
+            contacted.Remove(collider);
+        }
+    }
+}
